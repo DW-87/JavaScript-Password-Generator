6c71cf06de55a5a605733266bd3a3b5f7b514043
David Winters <davewinters1987@gmail.com>
2019-11-22 01:36:51 +0000

    Gen numbers + symbols

--- main.js
+++ main.js
@@ -10,4 +10,15 @@ function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
+// Gen Number
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// Gen Symbol
+function getRandomSymbol() {
+    const symbols = '!@#$^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 console.log(getRandomLower());
