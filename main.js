ff278addd1f07817c0dec6741f1e4dfe08c7e251
David Winters <davewinters1987@gmail.com>
2019-11-22 01:29:44 +0000

    Gen Uppercase

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
+// Gen Uppercase
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
 console.log(getRandomLower());
